698cb55ce07683ce6562960cc9930ebaeda9d406
stanleymaingi <stanleymaingi94@gmail.com>
2026-02-17 12:11:00 +0300

    Push clean api_project as regular folder

---
+++ api_project/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models # type: ignore
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
