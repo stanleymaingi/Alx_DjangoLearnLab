75baac56dfe65756f50868c52e05de050fda609f
stanleymaingi <stanleymaingi94@gmail.com>
2026-02-24 14:51:31 +0300

    add changes

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -153,3 +153,47 @@ def delete_comment(request, comment_id):
         messages.success(request, "Comment deleted successfully!")
         return redirect('post_detail', pk=comment.post.pk)
     return render(request, 'blog/delete_comment.html', {'comment': comment})
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Post, Comment
+from .forms import CommentForm
+
+# Create a comment (logged-in users only)
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/add_comment.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = Post.objects.get(pk=self.kwargs['post_id'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['post_id']})
+
+# Update a comment (only author can edit)
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/edit_comment.html'
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.get_object().post.pk})
+
+# Delete a comment (only author can delete)
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/delete_comment.html'
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.get_object().post.pk})
