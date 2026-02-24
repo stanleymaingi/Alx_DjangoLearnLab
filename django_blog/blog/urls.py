9d73cd6715cb2937ec385fa558497f45b95eaf88
stanleymaingi <stanleymaingi94@gmail.com>
2026-02-24 14:53:36 +0300

    add changes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . import views
+from .views import (
+    CommentCreateView,
+    CommentUpdateView,
+    CommentDeleteView,
+)
 
-urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile_view, name='profile'),
+urlpatterns += [
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add_comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='edit_comment'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete_comment'),
 ]
